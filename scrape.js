ca6d825a3622b5d2b348c27e674dfbf2a6b05299
Anoop G <mr.anoopg@gmail.com>
2019-06-15 22:58:01 +0400

    Scraping itunes category page and get all links

--- scrape.js
+++ scrape.js
@@ -2,6 +2,7 @@ const request = require('request');
 const xmlParser = require('xml2json');
 const express = require('express');
 const mysql = require('mysql');
+const cheerio = require('cheerio');
 
 const app = express();
 
@@ -22,19 +23,34 @@ app.listen('3000', () => {
     console.log('sever is running');
 });
 
-var categories = {
-    "arts": "https://podcasts.apple.com/us/genre/podcasts-music/id513611341?"
-}
+var allCategories = {};
 
-app.get('/getpodcast', (req, res) => {
+app.get('/getcategories', (req, res) => {
+    request('https://itunes.apple.com/us/genre/podcasts-health/id1307?', (error, response, html) => {
+        if (!error && response.statusCode == 200) {
+            const $ = cheerio.load(html);
 
-    for (category in categories) {
-        var urlChunks = categories[category].split('/');
-        var urlChunks_id = urlChunks[urlChunks.length - 1].slice(2, -1);
-        grabval(urlChunks_id);
+            const column_first = $('.grid3-column a');
+            $('.grid3-column a').each((i, el) => {
+                let catName = $(el).text();
+                let catHref = $(el).attr('href');
+
+                allCategories[catName] = catHref;
+            })
+
+            stripeId(allCategories);
+        }
+    });
+
+    function stripeId(categories) {
+        for (category in categories) {
+            var urlChunks = categories[category].split('/');
+            var urlChunks_id = urlChunks[urlChunks.length - 1].slice(2);
+            grabval(urlChunks_id);
+        }
     }
 
-    function grabval(id) {
+    function grabval(id) { 
         var url = 'https://itunes.apple.com/lookup?id=' + id;
         request(url, function (err, resp, body) {
             const val = JSON.parse(body);
@@ -49,18 +65,16 @@ app.get('/getpodcast', (req, res) => {
             const jsonval = JSON.parse(xmlval);
 
             for (var j = 0; j < jsonval.rss.channel.item.length; j++) {
-                console.log(jsonval.rss.channel.item[j])
 
-                let post = { title: jsonval.rss.channel.item[j].title, description: jsonval.rss.channel.item[j].description,pubDate:jsonval.rss.channel.item[j].pubDate,author:'author'+j,copyright:jsonval.rss.channel.item[j].copyright,music:jsonval.rss.channel.item[j].enclosure.url };
+                let post = { title: jsonval.rss.channel.item[j].title, description: jsonval.rss.channel.item[j].description, pubDate: jsonval.rss.channel.item[j].pubDate, author: jsonval.rss.channel.item[j]['itunes:author'], copyright: jsonval.rss.channel.item[j].copyright, music: jsonval.rss.channel.item[j].enclosure.url };
                 let sql = 'INSERT INTO library SET ?';
                 let query = db.query(sql, post, (err, result) => {
                     if (err) throw err;
-                    console.log('post '+jsonval.rss.channel.item[j].title+' added');
+                    console.log('post ' + jsonval.rss.channel.item[j].title + ' added');
                     res.send('Post 1 added...');
                 });
             }
         })
-    }
+    };
 
 });
-
